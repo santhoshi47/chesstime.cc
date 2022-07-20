3f70c94d0893d9323fc9189c56241b12345c64c3
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-07-20 10:19:50 +0530

    Create construct n.cc

---
+++ construct n.cc
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int t,x,y,n;
+		Scanner s=new Scanner(System.in);
+		t=s.nextInt();
+		{
+		    for(int i=0;i<t;i++)
+		    {
+		        n=s.nextInt();
+		        
+		        if(n==1||n==3||n==5)
+		        System.out.println("no");
+		        else
+		        System.out.println("yes");
+		        
+        }
+		}
+	}
+}
