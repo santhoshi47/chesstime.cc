e064a794c2d8188627f9810d672f6f0edb223a5d
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-07-25 18:49:28 +0530

    Create chef in his office.cc

---
+++ chef in his office.cc
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int t,x,y;
+		Scanner s=new Scanner(System.in);
+		t=s.nextInt();
+		for(int i=0;i<t;i++){
+		    x=s.nextInt();
+		    y=s.nextInt();
+		    System.out.println(x*4+y);
+		}
+		
+	}
+}
