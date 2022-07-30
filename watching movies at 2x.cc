b404e1ed022a0d007cf0cde16b308bf88730cf30
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-07-30 11:15:11 +0530

    Create watching movies at 2x.cc

---
+++ watching movies at 2x.cc
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x,y;
+	Scanner s=new Scanner(System.in);
+	    x=s.nextInt();
+	    y=s.nextInt();
+	  System.out.println(x-(y/2));
+
+}}
