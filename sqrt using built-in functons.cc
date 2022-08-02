d57656bed743fa95ee6de60bdfe7004d05e8813c
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-08-02 19:30:45 +0530

    Create sqrt using built-in functons.cc

---
+++ sqrt using built-in functons.cc
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    	Scanner input = new Scanner(System.in);
+    	int cases = input.nextInt();
+    	for (int i = 0 ; i < cases; i++){
+    	    int n = input.nextInt();
+    	    
+    	    System.out.println((int)Math.sqrt(n));
+    	} 
+	}
+}
+
