f7d6513313af4271b76ff252df72e634b132e798
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-08-22 11:59:01 +0530

    Create is it a vowel.cc

---
+++ is it a vowel.cc
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+char C;
+Scanner s=new Scanner(System.in);
+	    C=s.next().charAt(0);
+	    if(C=='A'||C=='E'||C=='I'||C=='O'||C=='U')
+	    System.out.print("Vowel");
+	    else
+	    System.out.print("Consonant");
+	}
+}
