e635af912f6b94e61b9b002542dc7ca35e805612
santhoshi47 <109609912+santhoshi47@users.noreply.github.com>
2022-09-06 11:12:07 +0530

    Create Degree of Polynmial.cc

---
+++ Degree of Polynmial.cc
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int t,n;
+int a=0,count=0;
+Scanner s=new Scanner(System.in);
+	    t=s.nextInt();
+	    for(int i=0;i<t;i++)
+	    {
+	        n=s.nextInt();
+	        for(int j=0;j<n;j++){
+	        a=s.nextInt();
+	            {
+	            if(a!=0)
+	            {
+	                count=j;
+	            }
+	        }}
+	        System.out.println(count);
+	    }}
+	
+}
